0ccbea30fd2c1960a6a8ba901222a0a45badeb7b
@it paradox <33118515+jgmcse@users.noreply.github.com>
2019-06-14 10:31:56 +0200

    Add files via upload
    Script to cipher user's password using AES 32bits Key

---
+++ UserPassword_cipher.ps1
@@ -0,0 +1,42 @@
+﻿<# =======================================================================
+
+File Name: UserPassword_cipher.ps1
+Author: itparadox@outlook.com (Spain)
+Date: 26-mar-2017
+
+Comments: Script to cipher user's password using AES 32bits Key
+
+======================================================================= #>
+
+Clear-Host
+
+$ScriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
+#-------------------------------------------------------------------
+# Request information from the user
+#-------------------------------------------------------------------
+$domain = Read-Host "Enter domain" # it is only useful if you need to cipher different domain user accounts.
+$User = Read-Host "Enter user"
+$password = Read-Host "Enter password" | ConvertTo-SecureString -AsPlainText -Force
+
+#-------------------------------------------------------------------
+# Define output file names
+#-------------------------------------------------------------------
+$KeyFile = $ScriptPath + "\" + $domain + "_" + $user + ".key"
+$PasswordFile = $ScriptPath + "\" + $domain + "_" + $user + ".pwd"
+
+
+#-------------------------------------------------------------------
+# AES 32bits Creation Key
+#-------------------------------------------------------------------
+$Key = New-Object Byte[] 32   # for AES
+[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($Key)
+$Key | out-file $KeyFile
+
+
+#-------------------------------------------------------------------
+# Password file creation using AES 32bits Key
+#-------------------------------------------------------------------
+$Key = Get-Content $KeyFile
+$password | ConvertFrom-SecureString -key $Key | Out-File $PasswordFile
+
